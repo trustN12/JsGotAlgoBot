38408b69abeacb317f752cf11c040fa4b90e99ac
NABARUN <nbiswas100@outlook.com>
2024-06-29 01:28:31 +0530

    Update askedByGoogle_numberOfGoodPairs.js

--- askedByGoogle_numberOfGoodPairs.js
+++ askedByGoogle_numberOfGoodPairs.js
@@ -32,15 +32,12 @@ Constraints:
 */
 
 
-
  function numIdenticalPairs(nums) {
   let count = 0;
 
-  for(let i=0; i<nums.length; i++){
+  for(let i=0; i<=nums.length; i++){
       for(let j=i+1; j<nums.length; j++){
-          if(nums[i] == nums[j]){
-              count++;
-          }
+          if(nums[i] == nums[j] && i<j) count++;
       }
   }
   return count;
